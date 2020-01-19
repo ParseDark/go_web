bb3382d1288a641c1dced34f6c7d82880dc617ac
Hawei <hawei@paypal.com>
2020-01-19 15:49:18 +0800

    feat: add Unite response.

--- handler/user/create.go
+++ handler/user/create.go
@@ -2,39 +2,45 @@ package user
 
 import (
 	"fmt"
-	"net/http"
 
+	. "github.com/PaserDark/go_web/handler"
 	"github.com/PaserDark/go_web/pkg/errno"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
 )
 
+// Create creates a new user account.
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-	var err error
+	var r CreateRequest
 	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
-	log.Debugf("Username is [%s], Password is [%s]", r.Username, r.Password)
+	admin2 := c.Param("username")
+	log.Infof("URL username: %s", admin2)
 
+	desc := c.Query("desc")
+	log.Infof("URL key param desc: %s", desc)
+
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content-Type: %s", contentType)
+
+	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
-		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can't not in db: x.x.x.x")).Add("This is add message")
-		log.Errorf(err, "get a error")
-	}
-
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("error type is ErrUserNotFound")
+		SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+		return
 	}
 
 	if r.Password == "" {
-		err = fmt.Errorf("Password is empty")
+		SendResponse(c, fmt.Errorf("password is empty"), nil)
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+
+	// Show the user information.
+	SendResponse(c, nil, rsp)
 }
